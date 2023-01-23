5c79f2ef909f7d4264e24fa578cf98e62763687f
damridfwdfwefd <alexdamsem2004@gmail.com>
2023-01-23 20:05:13 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+ public class Main {
+     public static double equation(double a, double b) {
+         return b / a;
+     }
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         // первый вариант
+         double a = scanner.nextDouble();
+
+     }
+ }
+    }
+}
